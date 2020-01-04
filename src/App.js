60ff69c27859b10cc60b58340741a7f6e3aaef25
Jose Cabral <jose12marte@yahoo.com>
2020-01-03 23:16:10 -0500

    added particles.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,46 @@ import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 import ImageLink from "./components/ImageLink";
 import Rank from "./components/Rank"
+import Particles from 'react-particles-js'
 
 function App() {
   return (
-    <div>
+    <div className = "App">
+        <Particles className = 'particles'
+          params={{
+            particles: {
+              number: {
+                value: 100,
+                density: {
+                  enable: true,
+                  value_area: 800
+                }
+              },
+              polygon: {
+              move: {
+                enable: true
+                
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable : true, mode: "repulse"
+                },
+                onclick: { enable: true, mode: "push"}
+
+              }
+            }
+          }
+        }
+    } />
+      
       <h1 style = {{textAlign: "center"}}>Hello there</h1>
       <Navigation />
       <Logo />
       <Rank/>
       <ImageLink />
-
     </div>
 
   );
